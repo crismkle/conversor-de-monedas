fb12e1056caa925796c8f4c5a9643b6baf3541ec
CristianM <crism.klee@gmail.com>
2024-04-26 09:36:03 -0300

    Crea e implementa clase Moneda

--- src/modules/Moneda.java
+++ src/modules/Moneda.java
@@ -1,2 +1,30 @@
-package modules;public class Moneda {
+package modules;
+
+public class Moneda {
+    String nombreAcronimo;
+    Double ratio;
+    String pais;
+
+
+    public Moneda(String nombreAcronimo, String pais) {
+        this.nombreAcronimo = nombreAcronimo;
+        this.pais = pais;
+    }
+
+    public void setRatio(MonedaExchange miMonedaEx) {
+        this.ratio = miMonedaEx.conversion_rate();
+    }
+
+    public String getNombreAcronimo() {
+        return nombreAcronimo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public Double calcularConversion(Double valor) {
+
+        return valor * this.ratio;
+    }
 }
