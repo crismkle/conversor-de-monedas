2d0969d3de485f50d3e9a13f86183a7ff708f2cc
CristianM <crism.klee@gmail.com>
2024-04-26 20:14:33 -0300

    Crea clase registro

--- src/registro/Registro.java
+++ src/registro/Registro.java
@@ -1,2 +1,33 @@
-package registro;public class Registro {
+package registro;
+
+import java.util.Date;
+
+public class Registro {
+    Date consultaDate;
+    Double valor;
+    String moneda_base;
+    String moneda_objetivo;
+    Double ratio;
+    String lastUpdate;
+
+    public Registro(Date consultaDate, Double valor, String moneda_base, String moneda_objetivo, Double ratio, String lastUpdate) {
+        this.consultaDate = consultaDate;
+        this.valor = valor;
+        this.moneda_base = moneda_base;
+        this.moneda_objetivo = moneda_objetivo;
+        this.ratio = ratio;
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro{" +
+                "consultaDate=" + consultaDate +
+                ", valor=" + valor +
+                ", moneda_base='" + moneda_base + '\'' +
+                ", moneda_objetivo='" + moneda_objetivo + '\'' +
+                ", ratio=" + ratio +
+                ", lastUpdate='" + lastUpdate + '\'' +
+                '}';
+    }
 }
